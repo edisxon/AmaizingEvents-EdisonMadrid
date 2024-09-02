c388c53490c09bb28fe3eb1761ff3530252ee783
edisxon <edisxon10@gmail.com>
2024-09-01 20:29:59 -0500

    arreglada posicion de checkboxes, y estilos de details entre otros

--- scripts/details.js
+++ scripts/details.js
@@ -7,20 +7,20 @@
 function renderEventDetails(event) {
     const eventDetailsContainer = document.querySelector(".details")
     eventDetailsContainer.innerHTML = `
-        <section class="w-50 navbar d-flex justify-content-around flex-wrap">
+        <section class="w-50 navbar d-flex justify-content-around flex-wrap detailCard">
           <div>
             <img class="detailsImg" src="${event.image}" alt="${event.name}">
           </div>
             
-          <div>
+          <div class="detailText">
             <h3 class="text-center detailsTitle">${event.name}</h3>
             <ul class="list-group p-4">
-              <li class="">date: ${event.date}</li>
-              <li class="">category: ${event.category}</li>
-              <li class="">place: ${event.place}</li>
-              <li class="">capacity: ${event.capacity}</li>
-              <li class="">${event.assistance ? 'assistance' : 'estimate'}: ${event.assistance || event.estimate}</li>
-              <li class="">price: $${event.price}</li>
+              <li class=""><span class="detailsSpan">date</span>: ${event.date}</li>
+              <li class=""><span class="detailsSpan">category</span>: ${event.category}</li>
+              <li class=""><span class="detailsSpan">place</span>: ${event.place}</li>
+              <li class=""><span class="detailsSpan">capacity</span>: ${event.capacity}</li>
+              <li class=""><span class="detailsSpan">${event.assistance ? 'assistance' : 'estimate'}</span>: ${event.assistance || event.estimate}</li>
+              <li class=""><span class="detailsSpan">price</span>: $${event.price}</li>
             </ul>
           </div>
         </section>`
@@ -30,7 +30,7 @@ function renderEventDetails(event) {
 
 const params = new URLSearchParams(window.location.search)
 const eventId = parseInt(params.get('id')) 
-console.log(eventId);
+
 
 
 
@@ -39,13 +39,11 @@ console.log(eventId);
 
 
 fetch("https://aulamindhub.github.io/amazing-api/events.json").then(res => res.json()).then(data => {
-  console.log(data);
+
   const events = data.events
-  console.log(events);
-  
+
   const evento = data.events.find(event => event._id === eventId)
-  console.log(evento);
-  
+
   renderEventDetails(evento)
 
 })
